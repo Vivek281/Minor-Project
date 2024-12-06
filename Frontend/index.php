c57aaf60bd39072b95e07666a78211c9c673bf6e
Vivek281 <vivekjha2062@gmail.com>
2024-12-06 10:13:43 +0545

    New Navbar

--- Frontend/index.php
+++ Frontend/index.php
@@ -3,7 +3,16 @@ include("includes/navbar.php");
 ?>
 
 
-<h1>HI</h1>
+<header>
+    <h2 class="logo">BookVerse</h2>
+    <nav class="navigation">
+        <a href="#">Home</a>
+        <a href="#">Categories</a>
+        <a href="#">List</a>
+        <a href="#">About</a>
+        <button class="btnLogin-popup">Login</button>
+    </nav>
+</header>
 
 
 

ef30bcef9c917a08201c4718a775466e95171568
Vivek281 <vivekjha2062@gmail.com>
2024-12-10 16:33:37 +0545

    Login and registration file made and removed some unnecessary codes

--- Frontend/includes/footer.php
+++ Frontend/includes/footer.php
@@ -1,3 +1,17 @@
 </body>
+<!-- script link for login form -->
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<!-- up to here -->
+
+<script src="asset/JS/login.js"></script>
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
--- Frontend/index.php
+++ Frontend/index.php
@@ -7,8 +7,70 @@ include("includes/navbar.php");
 
 
 
+<div class="wrapper">
+<span class="icon-close">
+            <ion-icon name="close"></ion-icon>
+        </span>
+    <div class="form-box login">
+        <h2>Login</h2>
+        <form action="#">
+            <div class="input-box">
+                <span class="icon">
+                <ion-icon name="mail"></ion-icon>
+                </span>
+                <input type="email" placeholder="Email" required>
+                <label for=""></label>
+            </div>
+            <div class="input-box">
+                <span class="icon">
+                <ion-icon name="lock-closed"></ion-icon>
+                </span>
+                <input type="password" placeholder="Password"required>
+                <label for=""></label>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox">Remember me</label>
+                <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" class="btn">Login</button>
+            <div class="register-link">
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>
+        </form>
+    </div>
+    <div class="form-box register" style="display: none;">
+            <h2>Register</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                    <ion-icon name="person"></ion-icon>
+                    </span>
+                    <input type="text" placeholder="Username" required>
+                    <label for=""></label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                    <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" placeholder="Email" required>
+                    <label for=""></label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                    <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" placeholder="Password" required>
+                    <label for=""></label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-link">
+                    <p>Already have an account? <a href="#">Login</a></p>
+                </div>
+        </div>
+</div>
 
-    
+
+
 
 
 

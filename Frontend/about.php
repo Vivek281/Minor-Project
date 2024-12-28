58ee7df73b7179a9d26862569b2366768ede19bd
Vivek281 <vivekjha2062@gmail.com>
2024-12-28 21:34:44 +0545

    Images added, discord link added, etc.

---
+++ Frontend/about.php
@@ -0,0 +1,53 @@
+<?php
+include("includes/navbar.php");
+?>
+<?php
+include("message.php");
+?>
+
+
+<div class="about-section">
+    <div class="about-container">
+        <h1>About Us</h1>
+        <div class="about-content">
+            <div class="mission">
+                <h2>Our Mission</h2>
+                <p>BookVerse is dedicated to making literature accessible to everyone, everywhere. We believe that knowledge should be boundless and reading should be effortless.</p>
+            </div>
+            
+            <div class="features">
+                <h2>What We Offer</h2>
+                <ul>
+                    <li>Vast collection of digital books across multiple genres</li>
+                    <li>24/7 access to your personal digital library</li>
+                    <li>User-friendly interface for seamless reading experience</li>
+                    <li>Regular updates with latest releases</li>
+                    <li>Personalized reading recommendations</li>
+                </ul>
+            </div>
+
+            <div class="contact-info">
+                <h2>Contact Us</h2>
+                <p>Have questions? We'd love to hear from you!</p>
+                <div class="contact-details">
+                    <p>Email: contact@bookverse.com</p>
+                    <p>Phone: (555) 123-4567</p>
+                    <div class="social-links">
+                        <a href="#" style="margin-top:3px;"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                        <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                        </svg></ion-icon></a>
+                        <a href="#" style="margin-top:3px;"><ion-icon name="logo-instagram"></ion-icon></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
+include("includes/footer.php");
+?>
--- Frontend/includes/navbar.php
+++ Frontend/includes/navbar.php
@@ -10,8 +10,8 @@ include('header.php');
         <a href="index.php">Home</a>
         <a href="#">Categories</a>
         <a href="#">List</a>
-        <a href="#">About</a>
-        <?php if(isset($_SESSION['auth_user'])):?>
+        <a href="about.php">About</a>
+    <?php if(isset($_SESSION['auth_user'])):?>
         <a href=""><?= $_SESSION['auth_user']['user_name'];?></a>
         <form action="logout.php" method="POST">
             <button type="submit" name="logout_btn">LogOut</button>
